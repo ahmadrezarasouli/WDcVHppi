b2339a0c480a184a28900ddc3bfd854d7ef9266d
ahmadrezarasouli <ahmadrezarasouli7377@gmail.com>
2026-07-18 03:56:18 -0700

    Update data_loader.py

--- src/preprocessing/data_loader.py
+++ src/preprocessing/data_loader.py
@@ -1,1 +1,21 @@
+import pandas as pd
 
+
+def load_excel(file_path, sheet_name=0):
+    return pd.read_excel(file_path, sheet_name=sheet_name)
+
+
+def load_csv(file_path, **kwargs):
+    return pd.read_csv(file_path, **kwargs)
+
+
+def load_tsv(file_path):
+    return pd.read_csv(file_path, sep="\t", header=None)
+
+
+def save_excel(df, file_path):
+    df.to_excel(file_path, index=False)
+
+
+def save_csv(df, file_path):
+    df.to_csv(file_path, index=False)
